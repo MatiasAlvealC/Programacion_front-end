c15bf9293df76979a10e51f9814ce6a555917143
MatiasA <matiasalvealc@gmail.com>
2024-04-30 15:54:23 -0400

    agregado mas de la clase7 javascript

--- clase7/app.js
+++ clase7/app.js
@@ -57,5 +57,104 @@ while (x<26){
 }
 
 
+// Array
 
+let array=[]
+
+for (i = 0;i <= 20; i++){
+    array.push(i);
+}
+
+console.log(array)
+
+// ejercicios
+// crear una funcion que permita mostrar el contenido de un arrglo
+
+
+function mostrar(array){
+    for (i=0; i<= array.length;i++){
+        console.log(array[i]);
+    }
+}
+
+let numeritos=[]
+for (i = 0;i <= 20; i++){
+    numeritos.push(i);
+}
+
+mostrar(numeritos)
+
+// método forEach
+// permite recorer arreglos
+
+let numeros=[2,4,6,8];
+
+numeros.forEach(function(elemento,i,arreglo){
+    arreglo[i]=Math.pow(elemento,2);
+})
+console.log(numeros);
+
+
+let suma=0;
+let numbers = [65,44,12,4];
+numbers.forEach(myFunction)
+
+function myFunction(item){
+    suma = suma + item;
+    document.write("<br>"+"valor : "+item+" total: "+suma)
+}
+
+// cree un arreglo con elementos aleatorios
+// Muestre el promedio de los avalores del arrglo
+// Muestre el mayor y el menor elemento del arreglo
+
+let aleatorio=[];
+
+for (i=0;i<=10;i++){
+    aleatorio[i]=Math.ceil((Math.random()* 100) + 1);
+    if (i==0){
+        aleatorioMaximo=aleatorio[i];
+        aleatorioMinimo=aleatorio[i];
+    } else {
+        if (aleatorio[i]>aleatorioMaximo){
+            aleatorioMaximo=aleatorio[i];
+        }
+        if (aleatorio[i]<aleatorioMinimo){
+            aleatorioMinimo=aleatorio[i];
+        }
+    }
+}
+console.log(aleatorio)
+let suma2=0;
+aleatorio.forEach (function(elemt){
+    suma2 += elemt;
+});
+
+let promedio = suma2/aleatorio.length;
+console.log("El promedio es: "+promedio)
+console.log("El numero maximo es: "+aleatorioMaximo);
+console.log("El numero minimo es: "+aleatorioMinimo);
+
+
+
+// Cree un areglo con 5 nombres de alumnos
+// y para cada alumno en otro areglo sus notas
+
+// muestre el nombre del alumno con nota mayor
+
+let alumnos=["Juan","Maria","Pedro","Diego","Paz"];
+let notas=[6.5,4.4,5.4,6.8,1.3];
+
+
+for (i=0;i<=notas.length;i++){
+    if(i==0){
+        posicion=i;
+    } else{
+        if (notas[i]>notas[posicion]){
+            posicion=i;
+        }
+    }
+}
+
+console.log("El alumno con mayor nota es: "+alumnos[posicion])
 
