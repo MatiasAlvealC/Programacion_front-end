16aa1827e40f120dbff70a0450e701703f7a9ee4
MatiasA <matiasalvealc@gmail.com>
2024-05-28 14:40:39 -0400

    agregado C3 ejercicio1

---
+++ C3/Ejercicio1/app.js
@@ -0,0 +1,16 @@
+function datos(formulario) {
+    const { correo, telefono } = formulario;
+    let persona={correo:correo.value,telefono:telefono.value}
+    if (persona.correo !== "" && persona.telefono !== "") {
+        if (persona.telefono.length ==10 && persona.correo.includes("@")){ // correo.includes("@")
+            mensaje = "";
+            document.getElementById('Error').innerHTML = mensaje;
+            alert('Acceso concedido');
+        } else {
+            mensaje = "Error en correo electrónico y/o número de teléfono";
+            document.getElementById('Error').innerHTML = mensaje;
+        }
+    } else {
+      alert('Datos vacios, ingrese dato');
+    }
+  }
