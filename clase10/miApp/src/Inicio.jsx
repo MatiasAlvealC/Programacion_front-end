63541ac3dbb938e3b8048a5d2e05830ad45c031c
MatiasA <matiasalvealc@gmail.com>
2024-06-04 14:52:03 -0400

    agregado clase 10 NODE]

---
+++ clase10/miApp/src/Inicio.jsx
@@ -0,0 +1,14 @@
+import imagen from './assets/pokemon.png'
+
+
+
+function Inicio(){
+  return (
+    <>
+    <h1>Hello my App</h1>
+    <h1>Con React y Vite</h1>
+    <img className='imagenriolu' src={imagen}/>
+    </>
+  )
+}
+export default Inicio
